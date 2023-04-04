b6fc0971f2c1750535a4dd4b364db10ce23a01d0
momin-thabet <momin.thabet@gmail.com>
2023-04-04 13:25:54 +0300

    bb

--- src/app/service/user/verify.js
+++ src/app/service/user/verify.js
@@ -2,68 +2,69 @@ const { Users, Verification } = require('../../../model');
 const jwt = require('../../../utils/jwt');
 
 module.exports.verifyEmail = async (data) => {
-  try {
-    const { code, _id } = data;
-    const user = await Users.findOne({ _id, isDeleted: false });
-    if (!user) {
-      return { code: 1, message: 'user.notFoundUser', data: null };
-    }
+	try {
+		const { code, _id } = data;
 
-    const verifyUser = await Verification.findOne({
-      code,
-      userId: _id,
-      email: user.email,
-    });
-    console.log(verifyUser);
+		const user = await Users.findOne({ _id, isDeleted: false });
+		if (!user) {
+			return { code: 1, message: 'user.notFoundUser', data: null };
+		}
 
-    if (!user) {
-      return { code: 1, message: 'user.notFoundUser' };
-    }
-    if (!verifyUser) {
-      return { code: 2, message: 'user.InvalidCode' };
-    }
+		const verifyUser = await Verification.findOne({
+			code,
+			userId: _id,
+			email: user.email,
+		});
+		console.log('verifyUser:', verifyUser);
 
-    await verifyUser.remove();
+		if (!user) {
+			return { code: 1, message: 'user.notFoundUser' };
+		}
+		if (!verifyUser) {
+			return { code: 2, message: 'user.InvalidCode' };
+		}
 
-    user.verifiedEmail = true;
-    await user.save();
+		await verifyUser.remove();
 
-    return {
-      code: 0,
-      message: 'commonSuccess.message',
-    };
-  } catch (error) {
-    console.log(error);
-    throw new Error(error);
-  }
+		user.verifiedEmail = true;
+		await user.save();
+
+		return {
+			code: 0,
+			message: 'commonSuccess.message',
+		};
+	} catch (error) {
+		console.log(error);
+		throw new Error(error);
+	}
 };
 
 module.exports.verifyRefreshToken = async (data) => {
-  const { refreshToken } = data;
-  try {
-    const token = jwt.verifyRefreshToken(refreshToken);
-    console.log(token);
-    if (!token) {
-      return { code: 1, message: 'user.invalidToken' };
-    }
-    const accessToken = jwt.createAccessToken({
-      id: token.data.id,
-      role: token.data.role,
-    });
-    const newRefreshToken = jwt.createRefreshToken({
-      id: token.data.id,
-      role: token.data.role,
-    });
-    return {
-      code: 0,
-      message: 'commonSuccess.message',
-      data: {
-        accessToken,
-        refreshToken: newRefreshToken,
-      },
-    };
-  } catch (error) {
-    console.log(error);
-    return { code: 1, message: 'user.invalidToken' };
-  }
+	const { refreshToken } = data;
+	try {
+		const token = jwt.verifyRefreshToken(refreshToken);
+		console.log(token);
+		if (!token) {
+			return { code: 1, message: 'user.invalidToken' };
+		}
+		const accessToken = jwt.createAccessToken({
+			id: token.data.id,
+			role: token.data.role,
+		});
+		const newRefreshToken = jwt.createRefreshToken({
+			id: token.data.id,
+			role: token.data.role,
+		});
+		return {
+			code: 0,
+			message: 'commonSuccess.message',
+			data: {
+				accessToken,
+				refreshToken: newRefreshToken,
+			},
+		};
+	} catch (error) {
+		console.log(error);
+		return { code: 1, message: 'user.invalidToken' };
+	}
 };

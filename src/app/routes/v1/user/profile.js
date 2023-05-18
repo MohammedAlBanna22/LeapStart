f80d1a2a6e814251f5b9b0a2b1a7ef9a437a2496
momin-thabet <momin.thabet@gmail.com>
2023-05-18 14:11:42 +0300

    adding edit profile todos

---
+++ src/app/routes/v1/user/profile.js
@@ -0,0 +1,22 @@
+/**TODO: this the users routes related to the user profile 
+  routes it should include 
+    1. get user by id 
+    2. get all users ==? if needed and add pagination ,search filter ...
+    3. the diff update user by id
+      ==> this is just a suggestion
+        a. name and bio 
+        b. Full Name, Phone Number, Country, Date Of Birth,
+         Specialty(category need better naming and make the same the name offer all the api)
+        c. photos updated
+
+
+
+
+        expert => d. expert related edits 
+          ==> 
+          1. categories => needs docs  ,salary ? send request to admin 
+
+          2. mount them over at expert and add work hours to them
+
+  
+*/

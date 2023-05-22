da30ed3f626a924922136a3ee7bfc444226e2e80
Mohammed AlBanna <mohammedalbanna7820@gmail.com>
2023-05-22 16:54:33 +0300

    ready

--- src/app/routes/v1/user/profile.js
+++ src/app/routes/v1/user/profile.js
@@ -24,8 +24,8 @@ const controller = require('../../../controller/user/profile');
 const isAuthenticated = require('../../../../utils/middleware/auth/auth');
 const router = express.Router();
 
-router.get('/getuser/:_id', controller.getUser); //isAuthenticated
-router.get('/getall', controller.getUsers); //isAuthenticatedVerified,
+router.get('/getuser/:_id',isAuthenticated,controller.getUser); 
+router.get('/getall',isAuthenticatedVerified,controller.getUsers); 
 router.put('/editdetails', isAuthenticated, controller.editDetails);
 
 

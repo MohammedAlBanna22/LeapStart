fcebb87822b4992b120146ea8dc35273c320f2be
momin-thabet <momin.thabet@gmail.com>
2023-06-25 05:20:16 +0300

    a

--- src/app/routes/v1/calender/index.js
+++ src/app/routes/v1/calender/index.js
@@ -1,3 +1,19 @@
 /**
- * TODO: this file gonna hold the routes needed to communicate with google calender
+* TODO: this file gonna hold the routes needed to communicate with google     
+    calender
+
+    I think there is a way to create a new calender instance in which we do things in top of 
+ * there gonna be 3 routes
+ * 1. this index and if we fo implement google calender this gonna hold the
+ *      auth routes;
+ * 2. expert
+ * 3. user
  */
+const express = require('express');
+const router = express.Router();
+
+router.use('/expert', require('./expert'));
+
+router.use('/user', require('./user'));
+
+module.exports = router;
--- src/app/routes/v1/session/index.js
+++ src/app/routes/v1/session/index.js
@@ -1,3 +1,10 @@
 // TODO: this should hold necessary actions on session as booking ...
 // we gonna integrate google calender to make it easier .
 // and integrate google meeting to hold the meeting in our site
+const express = require('express');
+const router = express.Router();
+
+router.use('/expert', require('./expert'));
+router.use('/user', require('./user'));
+
+module.exports = router;

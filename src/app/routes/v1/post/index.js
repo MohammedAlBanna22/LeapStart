36713cd827194bc00e660c39da0ac9e161ef104e
Mohammed AlBanna <mohammedalbanna7820@gmail.com>
2023-07-26 01:51:12 +0300

    ready

--- src/app/routes/v1/post/index.js
+++ src/app/routes/v1/post/index.js
@@ -2,14 +2,11 @@ const express = require('express');
 const controller = require('../../../controller/post');
 const isAuthenticated = require('../../../../utils/middleware/auth/auth');
 const router = express.Router();
-
-
 router.post('/',isAuthenticated, controller.Create);
-router.get('/getpost/:id', controller.getPost);///getuser/:_id
+router.get('/getpost/:id', controller.getPost);
 router.get('/:id',isAuthenticated,controller.getPosts);
 //router.put('/:id', controller.put);
 router.post('/:id',isAuthenticated, controller.delete);
-
 
 module.exports = router;
 

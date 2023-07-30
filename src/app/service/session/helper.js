7c69cdbf10b36faa994f5f81ffd66a6cc61a8f95
momin-thabet <momin.thabet@gmail.com>
2023-07-30 19:09:05 +0300

    user add session

--- src/app/service/session/helper.js
+++ src/app/service/session/helper.js
@@ -1,4 +1,5 @@
 const moment = require('moment');
+const { Session } = require('../../../model');
 
 function isDateWithinAvailableHours(givenDate, availableHours) {
 	// Step 1:Parse the given date string to a Moment.js object
@@ -10,6 +11,7 @@ function isDateWithinAvailableHours(givenDate, availableHours) {
 
 	const fromDate = moment(availableHours.fromDate);
 	const toDate = moment(availableHours.toDate);
+
 	const fromTime = moment(
 		`${dateOnly}T${availableHours.fromTime}`,
 		'YYYY-MM-DDTHH:mm'
@@ -19,18 +21,33 @@ function isDateWithinAvailableHours(givenDate, availableHours) {
 		'YYYY-MM-DDTHH:mm'
 	);
 
+	const dayOfWeek = dateToCheck.day().toString();
+
+	// console.log({
+	// 	dateToCheck,
+	// 	dateOnly,
+	// 	fromDate,
+	// 	toDate,
+	// 	fromTime,
+	// 	toTime,
+	// 	dayOfWeek,
+	// });
+
 	// Step 3: Check if the given date is within the date range
 	if (dateToCheck.isBetween(fromDate, toDate, null, '[]')) {
+		// console.log('date is alright');
 		// Step 4: Check if the time is within the working hours range
-
 		if (dateToCheck.isBetween(fromTime, toTime, null, '[]')) {
+			// console.log('time is okay');
 			// Step 5: Check if the day of the week is included in daysOfWork array
-			const dayOfWeek = dateToCheck.day().toString();
+
 			if (availableHours.daysOfWork.includes(dayOfWeek)) {
+				// console.log('days are good all is good');
 				return true;
 			}
 		}
 	}
+	// console.log('all is wrong');
 	return false;
 }
 
@@ -41,20 +58,34 @@ function isSessionValid(start, end, availableHours) {
 	);
 }
 
+function isSessionTimeBooked(givenDate, availableHours) {
+	const fromTime = moment(
+		`${dateOnly}T${availableHours.fromTime}`,
+		'YYYY-MM-DDTHH:mm'
+	);
+	const toTime = moment(
+		`${dateOnly}${availableHours.toTime}`,
+		'YYYY-MM-DDTHH:mm'
+	);
+}
+
 module.exports = {
 	isDateWithinAvailableHours,
 	isSessionValid,
 };
 
-const availableHours = {
-	daysOfWork: ['1', '2', '3', '4', '5'], // Assuming Monday to Friday are working days (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
-	fromTime: '08:00', // Working hours start from 8:00 AM
-	toTime: '17:00', // Working hours end at 5:00 PM
-	fromDate: '2023-07-20', // Available from July 20, 2023
-	toDate: '2023-07-30', // Available until July 30, 2023
-};
+// const givenTime = '2023-08-21T11:30:00.000Z'; // Replace with the given time in the format "yyyy-mm-ddThh:mm:000Z"
 
-const givenDate = '2023-07-25T13:30:00'; // July 25, 2023, 1:30 PM
-const end = '2023-07-25T14:30:00'; //
+// const availableHours = {
+// 	// Assuming Monday to Friday are working days (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+// 	daysOfWork: [0, 1, 2, 3, 4, 5],
+// 	fromTime: '08:00', // Working hours start from 8:00 AM
+// 	toTime: '17:00', // Working hours end at 5:00 PM
+// 	fromDate: '2023-07-20', // Available from July 20, 2023
+// 	toDate: '2023-07-30', // Available until July 30, 2023
+// };
 
-console.log(isSessionValid(givenDate, end, availableHours));
+// const givenDate = '2023-07-25T13:30:00'; // July 25, 2023, 1:30 PM
+// const end = '2023-07-25T14:30:00'; //
+
+// console.log(isSessionValid(givenDate, end, availableHours));
--- src/app/service/session/user.js
+++ src/app/service/session/user.js
@@ -1,12 +1,81 @@
 const { Expert, Session, User } = require('../../../model');
-const { isDateWithinAvailableHours } = require('./helper');
+const moment = require('moment');
+const { isSessionValid } = require('./helper');
+
 module.exports.post = async (user, body) => {
 	try {
-		const { expertId } = body;
+		const { expertId, sessionStart } = body;
+		const userId = user._id;
+
+		let start = moment(sessionStart);
+		let end = moment(sessionStart).add(1, 'hours');
 
 		let expert = await Expert.findOne({ _id: expertId, isDeleted: false });
 
-		return { code: 0, message: '', data: { expert } };
+		let availableHours = expert.availableHours;
+		let daysIndex = {
+			Sunday: 0,
+			Monday: 1,
+			Tuesday: 2,
+			Wednesday: 3,
+			Thursday: 4,
+			Friday: 5,
+			Saturday: 6,
+		};
+		availableHours.daysOfWork = availableHours.daysOfWork.map(
+			(day) => daysIndex[day]
+		);
+
+		// check available working hours of expert
+		if (!isSessionValid(start, end, availableHours)) {
+			return {
+				code: '1',
+				message: `expert doesn't work at this time`,
+			};
+		}
+
+		const existingSession = await Session.findOne({
+			$or: [{ expertId: expertId }, { userId: userId }],
+			status: { $nin: ['cancelled', 'rejected'] }, // Exclude sessions with 'cancelled' or 'rejected' status
+			startTime: { $lte: start }, // Check if the given time is after or equal to the startTime
+			endTime: { $gte: start }, // Check if the given time is before or equal to the endTime
+		});
+
+		if (existingSession.expertId.toString() === expertId.toString()) {
+			return {
+				code: '1',
+				message: 'expert have a session at this time',
+			};
+		}
+
+		if (existingSession.userId.toString() === userId.toString()) {
+			return {
+				code: '1',
+				message: 'User have a session at this time',
+			};
+		}
+
+		const session = await Session.create({
+			expertId,
+			userId,
+			startTime: start,
+			endTime: end,
+			status: 'pending',
+			payment: {
+				amount: expert.hourlyRate,
+			},
+		});
+		return {
+			code: 0,
+			message: 'hi this session got inserted',
+			data: { session },
+		};
+
+		// return { code: 0, message: 'valid date', data: '' };
+
+		// check if there is a session in this time in expert calendar
+
+		// check if there is a session in this time in user calendar
 	} catch (error) {
 		console.log(error);
 		throw new Error(error);
